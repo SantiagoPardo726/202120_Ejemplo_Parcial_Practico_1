ad42345aeffc72d9f00c13381c7dce3321bb9b2c
Santiago Pardo <s.pardob@uniandes.edu.co>
2021-09-29 20:47:07 -0500

    listo

--- src/main/java/co/edu/uniandes/dse/parcialejemplo/controllers/MedicoController.java
+++ src/main/java/co/edu/uniandes/dse/parcialejemplo/controllers/MedicoController.java
@@ -6,16 +6,17 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.uniandes.dse.parcialejemplo.dto.MedicoDTO;
 import co.edu.uniandes.dse.parcialejemplo.entities.MedicoEntity;
 import co.edu.uniandes.dse.parcialejemplo.services.MedicoService;
 
-@Controller
+@RestController
 @RequestMapping("/medicos")
 public class MedicoController {
 	@Autowired
